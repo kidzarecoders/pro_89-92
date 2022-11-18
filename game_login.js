242e47ce352ffe29f4587bc2843e15b423a29566
kidzarecoders <107684978+kidzarecoders@users.noreply.github.com>
2022-11-18 20:18:11 +1100

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function addUser(){
+    player1_name=document.getElementById("p1name").value;
+    player2_name=document.getElementById("p2name").value;
+    
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location.replace("game_page.html");
+}
